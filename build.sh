3daefb3cf4b2b94db56905f235898c9b01f16b91
John Estabrook <jestabro@vyos.io>
2024-11-20 13:18:35 -0600

    T6718: pin required packages in build.sh
    (adapted from commit bd71aa6b728f037ad5d85d9310f3380342714348)

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 DIR=$1
 
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#fc327ecd769fffd70817f26a60f3277fc3df5dfd -y'
+
 eval `opam config env`
 make clean
 make
