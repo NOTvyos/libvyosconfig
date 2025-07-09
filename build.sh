c473b7d11b5ab85265e62353fc60e91b4290be56
John Estabrook <jestabro@vyos.io>
2025-07-09 16:06:07 -0500

    T7246: use public repo for update commit hash for drop lexer rule

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 DIR=$1
 
-sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config git@github.com:VyOS-Networks/vyos1x-config.git#1174b4a952d7a984c89bdc6478d4083176f125dc -y'
+sudo sh -c 'eval $(opam env --root=/opt/opam --set-root) && opam pin add vyos1x-config https://github.com/vyos/vyos1x-config.git#b8d221356a11e172bba4fc54ecb1a0f3133bf12a -y'
 
 eval `opam config env`
 make clean
